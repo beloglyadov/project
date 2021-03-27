433f8aea1afe5c089af64c4839912fa34203c859
Beloglyadov Sergey <s.beloglyadov@egmail.com>
2021-03-28 01:22:18 +0300

    Site

---
+++ joomla/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-03-27 22:00:43 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-03-27T22:00:43+00:00	INFO 192.168.0.12	joomlafailure	Empty password not allowed.
