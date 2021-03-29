3dbce7735d9f4b9b67586c3b73eebc2a255b927c
Sergey Beloglyadov <s.belolgyadov@gmail.com>
2021-03-29 11:43:55 +0300

    Otus Project Exam

---
+++ site/joomla/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-03-28 20:58:38 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-03-28T20:58:38+00:00	INFO 192.168.0.12	joomlafailure	Empty password not allowed.
