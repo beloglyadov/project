eeb89e592a376a386cd60065a14e53a72e5b69de
Sergey Beloglyadov <77415855+beloglyadov@users.noreply.github.com>
2021-04-02 22:14:20 +0300

    Otus Exam Project

--- slave/shut-master.sh
+++ slave/shut-master.sh
@@ -8,7 +8,7 @@ if [[ $code != 'HTTP/1.1 200 OK'* ]]; then
     mysqldump --all-databases --events --routines --master-data=1 > ~/exam_db_new.sql
     #Останавливаем slave
     mysql -e "STOP SLAVE;"
-    #Копируем на новый master скрипт для установки и запускаем его
+    #Копируем на новый master скрипт для установки и настройки, даём права и запускаем его
     #После перезагрузки выполнить скрипт launch-mon.sh для запуска PROMETHEUS и ELK
     sshpass -p 123 scp ~/exam_db_new.sql ~/master-new.sh root@192.168.0.22:~/
     sshpass -p 123 ssh root@192.168.0.22 chmod +rx ~/master-new.sh 
