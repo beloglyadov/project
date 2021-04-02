ebb4577f42d4bb86087deb7220da3b78c0285bdd
Sergey Beloglyadov <77415855+beloglyadov@users.noreply.github.com>
2021-04-02 22:09:19 +0300

    Otus Exam Project

--- slave/slave.sh
+++ slave/slave.sh
@@ -52,6 +52,11 @@ EOF
 
 crontab < ~/crontab.txt 
 
+#После восстановления работы на новом master сервере включаем репликацию с ним
+cat < ~/repl_new.sql >> EOF
+"CHANGE MASTER TO MASTER_HOST='192.168.0.22', MASTER_USER='repl', MASTER_PASSWORD='OtusProject@$', MASTER_LOG_FILE='binlog.000001', MASTER_LOG_POS=1, GET_MASTER_PUBLIC_KEY = 1; START SLAVE"
+EOF
+
 #Отключаем SELINUX и FIREWALLD, перезапускаем для применения изменений SELINUX
 sed -i s/^SELINUX=.*$/SELINUX=disabled/ /etc/selinux/config
 systemctl disable firewalld --now
