3f4b628d4db3d9aa965e1b0c5566640dfe44124f
Sergey Beloglyadov <77415855+beloglyadov@users.noreply.github.com>
2021-04-05 13:08:19 +0300

    Otus Exam Project

--- master/master.sh
+++ master/master.sh
@@ -81,9 +81,8 @@ cd /tmp/project/elk-filebeat
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.11.2-x86_64.rpm
 sudo rpm -vi filebeat-7.11.2-x86_64.rpm
 
-mv /tmp/project/elk-filebeat/filebeat.yml /etc/filebeat/
-
 filebeat modules enable apache
+mv .filebeat.yml /etc/filebeat/
 systemctl enable filebeat --now
 
 #Отключаем SELINUX и FIREWALLD, перезапускаем для применения изменений SELINUX
