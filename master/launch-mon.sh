3437f2a129212499598e64d5e477de2d4337c13d
Sergey Beloglyadov <77415855+beloglyadov@users.noreply.github.com>
2021-04-05 23:24:58 +0300

    Otus Exam Project

--- master/launch-mon.sh
+++ master/launch-mon.sh
@@ -4,6 +4,8 @@ docker-compose up -d
 
 #Устанавливаем и запускаем Filebeat локально
 cd /tmp/project/elk-filebeat
+mv -f ./filebeat.yml /etc/filebeat/
+systemctl restart filebeat
 docker-compose up -d
 sleep 120
 filebeat setup
