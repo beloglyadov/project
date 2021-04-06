3af108c6fdf42ebf981e3faad6f681a1eb534270
Sergey Beloglyadov <77415855+beloglyadov@users.noreply.github.com>
2021-04-07 01:45:26 +0300

    Otus Exam Project

--- slave/shut-master.sh
+++ slave/shut-master.sh
@@ -2,6 +2,8 @@ code=$(curl -Is http://192.168.0.21/joomla/ | head -1)
 
 if [[ $code != 'HTTP/1.1 200 OK'* ]]; then
     
+    #Копируем папку с сайтом на резервный сервер
+    sshpass -p 123 scp -r root@192.168.0.21:/var/www/html/joomla ~/
     #Тушим главный сервер, чтобы не мешал
     sshpass -p 123 ssh root@192.168.0.21 shutdown now
     #Останавливаем slave
@@ -10,7 +12,7 @@ if [[ $code != 'HTTP/1.1 200 OK'* ]]; then
     mysqldump --all-databases --events --routines --master-data=1 > ~/exam_db_new.sql    
     #Копируем на новый master скрипт для установки и настройки, даём права и запускаем его
     #После перезагрузки выполнить скрипт launch-mon.sh для запуска PROMETHEUS и ELK
-    sshpass -p 123 scp ~/exam_db_new.sql ~/master-new.sh root@192.168.0.22:~/
+    sshpass -p 123 scp -r ~/exam_db_new.sql ~/master-new.sh ~/joomla root@192.168.0.22:~/
     sshpass -p 123 ssh root@192.168.0.22 chmod +rx ~/master-new.sh 
     sshpass -p 123 ssh root@192.168.0.22 ~/master-new.sh 1>~/script_master-new.log 2>~/error_master-new.log
     crontab -r
