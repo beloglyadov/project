6f0fbeb4c4d8faa91856761f33d4be62643585f9
Sergey Beloglyadov <77415855+beloglyadov@users.noreply.github.com>
2021-04-07 12:31:03 +0300

    Otus Exam Project

--- slave/shut-master.sh
+++ slave/shut-master.sh
@@ -2,19 +2,17 @@ code=$(curl -Is http://192.168.0.21/joomla/ | head -1)
 
 if [[ $code != 'HTTP/1.1 200 OK'* ]]; then
     
+    crontab -r
     #Останавливаем slave
     mysql -e "STOP SLAVE;"
-    #Копируем папку с сайтом на сервер реплики
-    sshpass -p 123 scp -r root@192.168.0.21:/var/www/html/joomla ~/
     #Тушим главный сервер, чтобы не мешал
     sshpass -p 123 ssh root@192.168.0.21 shutdown now
     #Дампим БД для передачи на новый master
-    mysqldump -h 192.168.0.21 -u repl -p Otus#Slave^5@ --all-databases --events --routines --master-data=1 > ~/exam_db_new.sql    
+    mysqldump --all-databases --events --routines --master-data=1 > ~/exam_db_new.sql    
     #Копируем на новый master скрипт для установки и настройки, даём права и запускаем его
     #После перезагрузки выполнить скрипт launch-mon.sh для запуска PROMETHEUS и ELK
-    sshpass -p 123 scp -r ~/exam_db_new.sql ~/master-new.sh ~/joomla root@192.168.0.22:~/
+    sshpass -p 123 scp -r ~/exam_db_new.sql ~/master-new.sh root@192.168.0.22:~/
     sshpass -p 123 ssh root@192.168.0.22 chmod +rx ~/master-new.sh 
     sshpass -p 123 ssh root@192.168.0.22 ~/master-new.sh 1>~/script_master-new.log 2>~/error_master-new.log
-    crontab -r
-
+    
 fi
